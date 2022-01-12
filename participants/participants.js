085296d09dc5914d3600a94f5d118f2cd41d1bc9
TrevorRezac <trev.rezac@gmail.com>
2022-01-11 17:40:44 -0800

    built html, grabbed DOM

--- participants/participants.js
+++ participants/participants.js
@@ -3,6 +3,10 @@ import { checkAuth, logout } from '../fetch-utils.js';
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#name-input');
+const ageInput = document.querySelector('#age-input');
+console.log(form, nameInput, ageInput);
 
 logoutButton.addEventListener('click', () => {
     logout();
--- workshops/workshops.js
+++ workshops/workshops.js
@@ -3,6 +3,8 @@ import { checkAuth, logout } from '../fetch-utils.js';
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const workshopsContainer = document.querySelector('.workshops-container');
+console.log(workshopsContainer);
 
 logoutButton.addEventListener('click', () => {
     logout();
