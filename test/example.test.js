1e47dd266889449dffd7eecd28977cc982f12fc9
TrevorRezac <trev.rezac@gmail.com>
2022-01-12 12:57:01 -0800

    completed TDD for renderParticipant and uploaded screenshot of passing test

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,26 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { renderParticipant } from '../render-utils.js';
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderParticipant(participant) takes a participant object and returns DOM elements - a p tag with a class and text content that includes a name and age', (expect) => {
     //Arrange
+
+    const participant = {
+        name: 'Bob',
+        age: 40,
+        workshop_id: 1,
+    };
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = '<p class="participant">Bob - Age: 40</p>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderParticipant(participant);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected, 'renderParticipant(participant) took in a participant object and retured a P tag with class participant and text content of Bob - Age: 40');
 });
