bf0ed218624696d44b064e4be53459f321caa11d
TrevorRezac <trev.rezac@gmail.com>
2022-01-11 17:16:03 -0800

    supaDB set up and linked, pages created, auth page works

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://tfsbulwivqbmrctueurz.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MTk0NTQxNiwiZXhwIjoxOTU3NTIxNDE2fQ.5tRT8PU8YNinrHIlFY8YfQdbcM9EvDRtcuiK3CPUEi8';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -16,7 +16,7 @@ export async function checkAuth() {
 
 export async function redirectIfLoggedIn() {
     if (await getUser()) {
-        location.replace('./other-page');
+        location.replace('./workshops');
     }
 }
 
